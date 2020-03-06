9c141a6912e41e17c5f0ddc71cef0cb1fea8d0f7
Mohamad Kassir <mohamad.kassir@ebiznext.com>
2020-03-06 12:18:39 +0100

    more tests

--- src/test/scala/com/ebiznext/comet/schema/generator/SchemaGenSpec.scala
+++ src/test/scala/com/ebiznext/comet/schema/generator/SchemaGenSpec.scala
@@ -3,6 +3,7 @@ package com.ebiznext.comet.schema.generator
 import java.io.File
 
 import com.ebiznext.comet.TestHelper
+import com.ebiznext.comet.schema.model.{Domain, Format}
 
 class SchemaGenSpec extends TestHelper {
 
@@ -11,6 +12,15 @@ class SchemaGenSpec extends TestHelper {
     SchemaGen.execute(getClass().getResource("/sample/SomeDomainTemplate.xlsx").getPath)
     val outputFile = new File(settings.comet.metadata + "/someDomain.yml")
     outputFile.exists() shouldBe true
+    val result = YamlSerializer.mapper.readValue(outputFile,classOf[Domain])
+    result.name shouldBe "someDomain"
+    result.schemas.size shouldBe 2
+    val schema1 = result.schemas.filter(_.name == "SCHEMA1").head
+    schema1.metadata.flatMap(_.format) shouldBe Some(Format.POSITION)
+    schema1.attributes.size shouldBe 19
+    val schema2 = result.schemas.filter(_.name == "SCHEMA2").head
+    schema2.metadata.flatMap(_.format) shouldBe Some(Format.DSV)
+    schema2.attributes.size shouldBe 19
   }
 
 }
