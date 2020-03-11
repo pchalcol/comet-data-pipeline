9d6da056009f02988e9bf7827ca6c624cd8ce812
Mohamad Kassir <mohamad.kassir@ebiznext.com>
2020-03-11 12:00:34 +0100

    fix test after rebase

--- src/test/scala/com/ebiznext/comet/schema/generator/PostEncryptSchemaGenSpec.scala
+++ src/test/scala/com/ebiznext/comet/schema/generator/PostEncryptSchemaGenSpec.scala
@@ -2,18 +2,10 @@ package com.ebiznext.comet.schema.generator
 
 import java.util.regex.Pattern
 
-import com.ebiznext.comet.config.Settings
+import com.ebiznext.comet.TestHelper
 import com.ebiznext.comet.schema.model._
-import com.typesafe.config.ConfigFactory
-import com.typesafe.scalalogging.StrictLogging
-import org.scalatest.{FlatSpec, Matchers}
 
-class PostEncryptSchemaGenTest extends FlatSpec with Matchers with StrictLogging {
-
-  implicit lazy val settings: Settings = Settings(ConfigFactory.load())
-  lazy val encryptionTypes =
-    settings.comet.encryptionTypes.map(elem => (elem._1.toUpperCase, elem._2))
-
+class PostEncryptSchemaGenTest extends TestHelper {
   val p1 = Position(0, 0, Some(Trim.BOTH))
   val p2 = Position(1, 5, Some(Trim.BOTH))
   val p3 = Position(6, 10, Some(Trim.BOTH))
@@ -27,48 +19,53 @@ class PostEncryptSchemaGenTest extends FlatSpec with Matchers with StrictLogging
   val a5 = Attribute("att5", position = Some(p5))
   val attributes = List(a1, a2, a3, a4, a5)
 
-  val positionMetadata = Metadata(Some(Mode.FILE), Some(Format.POSITION))
-  val nonPositionMetadata = Metadata(Some(Mode.FILE), Some(Format.DSV))
+  new WithSettings() {
+    lazy val encryptionTypes =
+      settings.comet.encryptionTypes.map(elem => (elem._1.toUpperCase, elem._2))
 
-  val withEncryptionSchema = Schema(
-    "schemaWithEncryption",
-    Pattern.compile("somefile.txt"),
-    attributes,
-    Some(positionMetadata),
-    None,
-    None,
-    None,
-    None
-  )
-  val noEncryptionSchema = withEncryptionSchema.copy(attributes = List(a1, a3, a5))
-  val nonPositionSchema = withEncryptionSchema.copy(metadata = Some(nonPositionMetadata))
+    val positionMetadata = Metadata(Some(Mode.FILE), Some(Format.POSITION))
+    val nonPositionMetadata = Metadata(Some(Mode.FILE), Some(Format.DSV))
 
-  "a schema of Position files with encrypted types" should "have a post encrypted schema with correctly shifted positions" in {
-    val postEncryptSchema = PostEncryptSchemaGen.buildPostEncryptionSchema(withEncryptionSchema)
-    postEncryptSchema shouldBe defined
-    val attributes = postEncryptSchema.map(_.attributes)
-    attributes shouldBe defined
-    attributes.head.flatMap(_.position).map(_.first) shouldBe List(0, 1, 51, 56, 106)
-    attributes.head.flatMap(_.position).map(_.last) shouldBe List(0, 50, 55, 105, 112)
-  }
+    val withEncryptionSchema = Schema(
+      "schemaWithEncryption",
+      Pattern.compile("somefile.txt"),
+      attributes,
+      Some(positionMetadata),
+      None,
+      None,
+      None,
+      None
+    )
+    val noEncryptionSchema = withEncryptionSchema.copy(attributes = List(a1, a3, a5))
+    val nonPositionSchema = withEncryptionSchema.copy(metadata = Some(nonPositionMetadata))
 
-  "a shema with a format different than Position" should "not have a post encrypted schema" in {
-    PostEncryptSchemaGen.buildPostEncryptionSchema(nonPositionSchema) shouldBe empty
-  }
+    "a schema of Position files with encrypted types" should "have a post encrypted schema with correctly shifted positions" in {
+      val postEncryptSchema = PostEncryptSchemaGen.buildPostEncryptionSchema(withEncryptionSchema)
+      postEncryptSchema shouldBe defined
+      val attributes = postEncryptSchema.map(_.attributes)
+      attributes shouldBe defined
+      attributes.head.flatMap(_.position).map(_.first) shouldBe List(0, 1, 51, 56, 106)
+      attributes.head.flatMap(_.position).map(_.last) shouldBe List(0, 50, 55, 105, 112)
+    }
 
-  "a shema with a Position format and encrypted type" should "not have a post encrypted schema" in {
-    PostEncryptSchemaGen.buildPostEncryptionSchema(noEncryptionSchema) shouldBe empty
-  }
+    "a shema with a format different than Position" should "not have a post encrypted schema" in {
+      PostEncryptSchemaGen.buildPostEncryptionSchema(nonPositionSchema) shouldBe empty
+    }
 
-  "a list of attributes with encryption types" should "have a non empty Shift Commands" in {
-    PostEncryptSchemaGen
-      .buildShiftCommands(attributes, encryptionTypes)
-      .filter(_.isDefined) should not be empty
-  }
+    "a shema with a Position format and encrypted type" should "not have a post encrypted schema" in {
+      PostEncryptSchemaGen.buildPostEncryptionSchema(noEncryptionSchema) shouldBe empty
+    }
 
-  "a list of attributes with no encryption types" should "have an empty Shift Commands" in {
-    PostEncryptSchemaGen
-      .buildShiftCommands(List(a1, a3, a5), encryptionTypes)
-      .filter(_.isDefined) shouldBe empty
+    "a list of attributes with encryption types" should "have a non empty Shift Commands" in {
+      PostEncryptSchemaGen
+        .buildShiftCommands(attributes, encryptionTypes)
+        .filter(_.isDefined) should not be empty
+    }
+
+    "a list of attributes with no encryption types" should "have an empty Shift Commands" in {
+      PostEncryptSchemaGen
+        .buildShiftCommands(List(a1, a3, a5), encryptionTypes)
+        .filter(_.isDefined) shouldBe empty
+    }
   }
 }
